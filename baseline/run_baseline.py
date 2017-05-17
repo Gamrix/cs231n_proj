d7b4787f2b589ef4c34f4ac400606941cfda39e9
Matthew Volk <mvolk@stanford.edu>
2017-05-16 18:07:23 -0700

    Add completed run_baseline and final baseline run output

--- baseline/run_baseline.py
+++ baseline/run_baseline.py
@@ -1,6 +1,6 @@
 import glob
 import os
-from scipy.misc import imread
+from scipy.misc import imread, imsave
 
 import torch
 import torch.nn as nn
@@ -16,13 +16,14 @@ import torchvision.transforms as T
 import numpy as np
 
 NUM_TRAIN = 192
-NUM_VAL = 32
+NUM_VAL = 16
+BATCH_SIZE = 16
 DATA_DIR = "../src/preprocess/prep_res"
 PRINT_EVERY = 1
 
 NUM_EPOCHS = 3
 DROPOUT = 0.15
-INIT_LR = 5e-4
+INIT_LR = 2e-3
 
 class ChunkSampler(sampler.Sampler):
     """Samples elements sequentially from some offset. 
@@ -67,8 +68,8 @@ def make_loaders(inputs, gold):
     inputs_t = torch.from_numpy(inputs).byte()
     gold_t = torch.from_numpy(gold).byte()
     dataset = TensorDataset(inputs_t, gold_t)
-    train = DataLoader(dataset, batch_size=16, sampler=ChunkSampler(NUM_TRAIN, 0))
-    val = DataLoader(dataset, batch_size=16, sampler=ChunkSampler(NUM_VAL, NUM_TRAIN))
+    train = DataLoader(dataset, batch_size=BATCH_SIZE, sampler=ChunkSampler(NUM_TRAIN, 0))
+    val = DataLoader(dataset, batch_size=BATCH_SIZE, sampler=ChunkSampler(NUM_VAL, NUM_TRAIN))
     test = None # For now
 
     return train, val, test
@@ -98,8 +99,13 @@ def eval(model, dev_data, loss_fn):
     for t, (x, y) in enumerate(dev_data):
         x_var = Variable(x.float().permute(0,3,1,2))
         y_var = Variable(y.float().permute(0,3,1,2))
-
+        
         scores = model(x_var)
+        print (scores.size())
+        for i in range(scores.size()[0]):
+            name = "./eval/{}_{}_".format(t, i)
+            imsave(name + "gen.png", np.transpose(scores[i].data.numpy(), axes=[1,2,0]))
+            imsave(name + "gold.png", np.transpose(y_var[i].data.numpy(), axes=[1,2,0]))
         
         total_loss += loss_fn(scores, y_var).data[0]
 
@@ -109,28 +115,23 @@ def eval(model, dev_data, loss_fn):
 def run_model(train_data, val_data, test_data):
     model_base = nn.Sequential (
         # Conv 1
-        nn.Conv2d(6, 32, kernel_size=3, stride=1, padding=(1,1)), # out 32 * 224 * 224
-        nn.BatchNorm2d(32),
+        nn.Conv2d(6, 32, kernel_size=3, stride=1, padding=(1,1), bias=True), # out 32 * 224 * 224
         nn.ReLU(inplace=True),
         nn.Dropout2d(p=DROPOUT),
         # Conv 2
-        nn.Conv2d(32, 64, kernel_size=3, stride=1, padding=(1,1)), # out 64 * 224 * 224
-        nn.BatchNorm2d(64),
+        nn.Conv2d(32, 64, kernel_size=3, stride=1, padding=(1,1), bias=True), # out 64 * 224 * 224
         nn.ReLU(inplace=True),
         nn.Dropout2d(p=DROPOUT),
         # Conv 3
-        nn.Conv2d(64, 32, kernel_size=3, stride=1, padding=(1,1)), # out 32 * 224 * 224
-        nn.BatchNorm2d(32),
+        nn.Conv2d(64, 32, kernel_size=3, stride=1, padding=(1,1), bias=True), # out 32 * 224 * 224
         nn.ReLU(inplace=True),
         nn.Dropout2d(p=DROPOUT),
         # Conv 4
-        nn.Conv2d(32, 16, kernel_size=3, stride=1, padding=(1,1)), # out 16 * 224 * 224
-        nn.BatchNorm2d(16),
+        nn.Conv2d(32, 16, kernel_size=3, stride=1, padding=(1,1), bias=True), # out 16 * 224 * 224
         nn.ReLU(inplace=True),
         nn.Dropout2d(p=DROPOUT),
         # Conv 5
-        nn.Conv2d(16, 3, kernel_size=3, stride=1, padding=(1,1)), # out 3 * 224 * 224
-        nn.BatchNorm2d(3),
+        nn.Conv2d(16, 3, kernel_size=3, stride=1, padding=(1,1), bias=True), # out 3 * 224 * 224
         nn.ReLU(inplace=True),
     )
     
