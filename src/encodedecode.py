9ce67ed9551a38d7a1a2f8b34faea83be5f76ebe
Brendan Corcoran <BrendanCorcoran@DN0a22e3dd.SUNet>
2017-05-30 23:27:39 -0700

    completed encode/decode

--- src/encodedecode.py
+++ src/encodedecode.py
@@ -12,7 +12,11 @@ import numpy as np
 
 import timeit
 
-
+"""
+Example usage:
+ed = EncodeDecode()
+M1, M2, C = ed(im1, im2)
+"""
 class EncodeDecode(nn.Module):
     def __init__(self):
         self.ec3 = nn.Sequential(
@@ -25,14 +29,26 @@ class EncodeDecode(nn.Module):
             nn.Conv2d(64, 128, kernel_size=5, stride=1),
             nn.ReLU(inplace=True))
 
+        self.ec3feature = nn.Sequential(
+            nn.Conv2d(128, 64, kernel_size=1, stride=1),
+            nn.ReLU(inplace=True))
+
         self.ec4 = nn.Sequential(
             nn.MaxPool2d(kernel_size=3, stride=2),
             nn.Conv2d(128, 256, kernel_size=3, stride=1),
             nn.ReLU(inplace=True))
 
+        self.ec4feature = nn.Sequential(
+            nn.Conv2d(256, 128, kernel_size=1, stride=1),
+            nn.ReLU(inplace=True))
+
         self.ec5 = nn.Sequential(
             nn.MaxPool2d(kernel_size=3, stride=2),
             nn.Conv2d(256, 512, kernel_size=3, stride=1),
+            nn.ReLU(inplace=True))
+
+        self.ec5feature = nn.Sequential(
+            nn.Conv2d(512, 256, kernel_size=1, stride=1),
             nn.ReLU(inplace=True))
 
         self.ec6 = nn.Sequential(
@@ -44,7 +60,45 @@ class EncodeDecode(nn.Module):
             nn.Conv2d(1024, 1024, kernel_size=1, stride=1),
             nn.ReLU(inplace=True),
             nn.Conv2d(1024, 1024, kernel_size=1, stride=1),
+            nn.ReLU(inplace=True),
+            nn.ConvTranspose2d(1024, 512, kernel_size=4, stride=2),
+            nn.ReLU(inplace=True),
+            nn.ConvTranspose2d(512, 256, kernel_size=4, stride=2),
+            nn.ReLU(inplace=True),
+            nn.ConvTranspose2d(256, 128, kernel_size=4, stride=2),
+            nn.ReLU(inplace=True),
+            nn.ConvTranspose2d(128, 64, kernel_size=4, stride=2),
+            nn.ReLU(inplace=True),
+            nn.ConvTranspose2d(64, 32, kernel_size=4, stride=2),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(32, 1, kernel_size=3, stride=1),
+            nn.Sigmoid())
+
+        self.cd1 = nn.Sequential(
+            nn.Conv2d(1024, 2048, kernel_size=1, stride=1),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(2048, 2048, kernel_size=1, stride=1),
+            nn.ReLU(inplace=True),
+            nn.ConvTranspose2d(2048, 768, kernel_size=4, stride=2),
             nn.ReLU(inplace=True))
+
+        self.cd2 = nn.Sequential(
+            nn.ConvTranspose2d(1024, 384, kernel_size=4, stride=2),
+            nn.ReLU(inplace=True)
+            )
+
+        self.cd3 = nn.Sequential(
+            nn.ConvTranspose2d(512, 192, kernel_size=4, stride=2),
+            nn.ReLU(inplace=True)
+            )
+
+        self.cc3 = nn.Sequential(
+            nn.ConvTranspose2d(256, 128, kernel_size=4, stride=2),
+            nn.ReLU(inplace=True),
+            nn.ConvTranspose2d(128, 64, kernel_size=4, stride=2),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(64, 2, kernel_size=3, stride=1)
+            )
 
 
     def forward(self, im1, im2):
@@ -56,8 +110,15 @@ class EncodeDecode(nn.Module):
 
         M1 = self.vc3sig(ec6)
 
-        return self.model(concat)
+        cd1 = self.cd1(ec6)
+        ec5feature = self.ec5feature(ec5)
+        cd1ec5feature = torch.cat((cd1, ec5), dim=1)
+        cd2 = self.cd2(cd1ec5feature)
+        ec4feature = self.ec4feature(ec4)
+        cd2ec4feature = torch.cat((cd2, ec4), dim=1)
+        cd3 = self.cd3(cd2ec4feature)
+        ec3feature = self.ec3feature(ec3)
+        cd3ec3feature = torch.cat((cd3, ec3), dim=1)
+        C = self.cc3(cd3ec3feature)
 
-
-ed = EncodeDecode()
-M1, M2, C = ed(im1, im2)
+        return M1, (1 - M1), C
