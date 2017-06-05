Correct mask normalization.

src/viewmorphing.py
@@ -68,7 +68,6 @@ class ViewMorphing(nn.Module):
         oob_loss = torch.mean((qi_rescale - qi) ** 2) / self.image_dim ** 2 * 0.0001
         res_img = res_img_flat.view_as(image)
 
-        mask = mask / torch.sum(mask).expand_as(mask)
         return res_img * mask.expand_as(res_img), oob_loss
 
     def forward(self, arglist):
@@ -78,8 +77,12 @@ class ViewMorphing(nn.Module):
         print("max: {} \n min: {}".format(Cflat.max(), Cflat.min()))
         # cflat is supposed to be between -1 and 1
 
-        a, oob_loss_a = self.get_masked_RP(im1, M1, self.q.expand_as(Cflat) + Cflat)
-        b, oob_loss_b = self.get_masked_RP(im2, M2, self.q.expand_as(Cflat) - Cflat)
+        total_mask = M1 + M2
+        M1_new = M1 / total_mask
+        M2_new = M2 / total_mask
+
+        a, oob_loss_a = self.get_masked_RP(im1, M1_new, self.q.expand_as(Cflat) + Cflat)
+        b, oob_loss_b = self.get_masked_RP(im2, M2_new, self.q.expand_as(Cflat) - Cflat)
 
         return a + b, oob_loss_a + oob_loss_b
 